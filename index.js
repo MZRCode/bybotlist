fd042ec56cfde1d3972c30affae7072fda8a3433
mzrdev <87870017+MZRCode@users.noreply.github.com>
2023-10-22 15:50:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -175,7 +175,7 @@ if (interaction.customId === "onayla") {
     }
   
     const author = { name: `${a.username} | Onaylandı`, iconURL: a.displayAvatarURL({ format: "png", size: 1024 }) };
-    const uyecik = await guild.members.fetch(bot);
+    const botcuk = await guild.members.fetch(bot);
 
     const yetkili = member.user;
     
